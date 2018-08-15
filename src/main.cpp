e9e559a802071597c60824723eba64d97cfb9a88
russ <russellsmith83@gmail.com>
2018-08-16 00:06:11 +0100

    add icon and remove console

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include "app.h"
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
 
 int main() {
+	App *app = new App;
+	app->run();
+}
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow) {
 	App *app = new App;
 	app->run();
 }
